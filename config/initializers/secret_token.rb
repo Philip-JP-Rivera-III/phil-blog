973969fcb7ad2703c0e742fa31c73f1d64e02e3a
Elisa <elisa@DNG46DF654H5DFR.(none)>
2013-04-22 17:54:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhilBlog::Application.config.secret_token = '8212e64922a6016e1c35a07f903d3b3dc325e2046ccc7bd56f18a8d35e41804e9cf18424cf732e1fed17e0f632f6fd75c120c1e68d8242b35f52fc7942c596e1'
